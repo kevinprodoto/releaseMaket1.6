fb408e82a5e31fdec6965477c283072ed965e8b7
kevinprodoto <vitalik03031998@gmail.com>
2020-08-30 16:50:57 +0300

    Yes

--- read-more-main.js
+++ read-more-main.js
@@ -3,8 +3,8 @@ let iconMain = document.querySelector(".read-more__icon");
 let textLaptop = document.querySelector(".main__text--laptop");
 let textDesktop = document.querySelector(".main__text--desktop");
 readMoreMain.addEventListener("click", function() {
-    textLaptop.classList.toggle("main__text--laptop");
-    textDesktop.classList.toggle("main__text--desktop");
+    textLaptop.classList.toggle("text-active");
+    textDesktop.classList.toggle("text-active");
     iconMain.classList.toggle("rotate");
     if (readMoreMain.textContent === "Читать далее") {
        readMoreMain.textContent = "Скрыть";
